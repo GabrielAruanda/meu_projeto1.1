d367b9aa65174c879408705f014baff221f39f79
GabrielAruanda <garuanda@gmail.com>
2024-07-11 11:39:14 -0300

    Arualização e graficos

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_mysqldb import MySQL
 from config import Config
 import random
 import string
-import hashlib  # Importa a biblioteca hashlib para codificar a senha
-from werkzeug.security import generate_password_hash, check_password_hash  # Importa funções de hash do Werkzeug
-import requests  # Importa a biblioteca requests para obter o IP público do usuário
+import hashlib  
+from werkzeug.security import generate_password_hash, check_password_hash  
+import requests  
+import json
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-# Configuração inicial do Flask
 app = Flask(__name__)
-app.config.from_object(Config)  # Carrega as configurações do arquivo Config
-app.secret_key = Config.SECRET_KEY  # Define a chave secreta para sessões
+app.config.from_object(Config)  
+app.secret_key = Config.SECRET_KEY  
 
-# Configuração do MySQL
 mysql = MySQL(app)
 
-# Função para gerar URL curta aleatória
 def generate_short_url(original_url):
-    # Extrai os primeiros 3 caracteres do final da URL original
     url_name = original_url.rstrip('/').rsplit('/', 1)[-1][:3]
-    # Gera uma parte aleatória com 3 números
     random_part = ''.join(random.choices(string.digits, k=3))
-    # Retorna a URL curta formada pela combinação dos elementos
     return f"{url_name}{random_part}"
 
-# Função para obter o endereço IP público do usuário
 def get_public_ip():
     try:
         response = requests.get('https://api64.ipify.org?format=json')
@@ -34,24 +30,15 @@ def get_public_ip():
         print(f"Erro ao obter IP público: {e}")
         return None
 
-# Rota para redirecionar URL encurtada
 @app.route('/<short_url>')
 def redirect_to_url(short_url):
     cur = mysql.connection.cursor()
-    # Busca a URL original correspondente à URL curta fornecida
     cur.execute("SELECT original_url FROM urls WHERE short_url = %s", (short_url,))
     result = cur.fetchone()
     if result:
-        # Incrementa o contador de cliques da URL
         cur.execute("UPDATE urls SET click_count = click_count + 1 WHERE short_url = %s", (short_url,))
-        
-        # Obtém o endereço IP local do usuário
         local_ip = request.remote_addr
-        
-        # Obtém e salva o endereço IP público do usuário
         public_ip = get_public_ip()
-        
-        # Registra o clique na tabela de clicks associando ao usuário logado, se houver
         user_id = session.get('user_id')
         if user_id:
             cur.execute("""
@@ -66,37 +53,30 @@ def redirect_to_url(short_url):
         
         mysql.connection.commit()
         cur.close()
-        # Redireciona para a URL original
         return redirect(result[0])
     else:
-        # Exibe mensagem de erro se a URL curta não for encontrada
         flash('URL encurtada não encontrada.', 'danger')
         return redirect(url_for('index'))
 
-# Rota para a página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Rota para a página home
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    if 'logged_in' in session:  # Verifica se o usuário está logado na sessão
-        user_id = session['user_id']  # Obtém o ID do usuário da sessão
-        username = session['username']  # Obtém o nome de usuário da sessão
+    if 'logged_in' in session:
+        user_id = session['user_id']
+        username = session['username']
         if request.method == 'POST':
             original_url = request.form['original_url']
             cur = mysql.connection.cursor()
             try:
-                # Verifica se a URL original já foi encurtada pelo usuário
                 cur.execute("SELECT short_url FROM urls WHERE original_url = %s AND user_id = %s", (original_url, user_id))
                 existing_short_url = cur.fetchone()
                 if existing_short_url:
-                    # Se já existe, retorna a URL curta correspondente
                     short_url = existing_short_url[0]
                     flash(f'URL encurtada já existente: {request.url_root}{short_url}', 'info')
                 else:
-                    # Senão, gera uma nova URL curta e a insere no banco de dados
                     short_url = generate_short_url(original_url)
                     cur.execute("""
                         INSERT INTO urls (original_url, short_url, user_id)
@@ -105,27 +85,21 @@ def home():
                     mysql.connection.commit()
                     flash(f'URL encurtada: {request.url_root}{short_url}', 'success')
             except Exception as e:
-                # Exibe mensagem de erro se ocorrer algum problema na inserção
                 flash(f'Erro ao encurtar a URL: {e}', 'danger')
             cur.close()
         
-        # Obtém todas as URLs encurtadas do usuário logado
         cur = mysql.connection.cursor()
         cur.execute("SELECT id, original_url, short_url, click_count FROM urls WHERE user_id = %s", (user_id,))
         urls = cur.fetchall()
         cur.close()
 
-        # Ordena as URLs pelo número de cliques em ordem decrescente
         urls = sorted(urls, key=lambda x: x[3], reverse=True)
         
-        # Renderiza a página home com as URLs do usuário
         return render_template('home.html', username=username, logged_in=True, urls=urls)
     else:
-        # Se o usuário não está logado, redireciona para a página inicial com mensagem de aviso
         flash('Você precisa estar logado para acessar esta página.', 'danger')
         return redirect(url_for('index'))
 
-# Rotas para outras páginas estáticas
 @app.route('/features')
 def features():
     return render_template('features.html')
@@ -144,14 +118,12 @@ def contact():
         return redirect(url_for('contact'))
     return render_template('contact.html')
 
-# Rota para registro de usuário
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         
-        # Codifica a senha antes de salvar no banco de dados
         hashed_password = generate_password_hash(password)
         
         cur = mysql.connection.cursor()
@@ -162,7 +134,6 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-# Rota para login de usuário
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -172,22 +143,18 @@ def login():
         cur.execute("SELECT * FROM users WHERE username = %s", (username,))
         user = cur.fetchone()
         cur.close()
-        if user and check_password_hash(user[2], password):  # Verifica a senha hashada
-            # Se o login for bem-sucedido, define as variáveis de sessão
+        if user and check_password_hash(user[2], password):
             session['logged_in'] = True
             session['username'] = username
-            session['user_id'] = user[0]  # ID do usuário na posição 0 da tupla
+            session['user_id'] = user[0]
             flash('Login realizado com sucesso!', 'success')
-            return redirect(url_for('home'))  # Redireciona para a página home
+            return redirect(url_for('home'))
         else:
-            # Exibe mensagem de erro se o login falhar
             flash('Usuário ou senha incorretos.', 'danger')
     return render_template('login.html')
 
-# Rota para logout
 @app.route('/logout')
 def logout():
-    # Remove as variáveis de sessão ao fazer logout
     session.pop('logged_in', None)
     session.pop('username', None)
     session.pop('user_id', None)
@@ -202,17 +169,36 @@ def privacy():
 def terms():
     return render_template('terms.html')
 
-# Rotas para as páginas de contratação dos planos, passando o nome do plano na URL
 @app.route('/contratar_form/<plano>', methods=['GET', 'POST'])
 def contratar_form(plano):
     if request.method == 'POST':
-        # Processa os dados do formulário de contratação
         flash(f'Contratação do {plano} realizada com sucesso!', 'success')
         return redirect(url_for('pagina_preco', plano=plano))
     return render_template('contratar_form.html', plano=plano)
 
+# Rota para renderizar a página do gráfico
+@app.route('/grafic')
+def mostrar_grafico():
+    return render_template('grafic.html')
 
+# Rota para obter os dados de cliques
+@app.route('/get_click_data')
+def get_click_data():
+    if 'logged_in' in session:
+        user_id = session['user_id']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            SELECT urls.original_url, COUNT(clicks.id) as click_count
+            FROM clicks
+            JOIN urls ON clicks.url_id = urls.id
+            WHERE urls.user_id = %s
+            GROUP BY urls.original_url
+        """, (user_id,))
+        click_data = cur.fetchall()
+        cur.close()
+        return jsonify({'click_data': click_data})
+    else:
+        return jsonify({'click_data': []})
 
-# Roda o aplicativo Flask
 if __name__ == '__main__':
     app.run(debug=True)
